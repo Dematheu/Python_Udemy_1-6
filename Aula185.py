6f0db86f0f032b73b046b7b72a9a069da1756e16
Dematheu <arq.dematheu@gmail.com>
2023-05-25 17:44:13 -0300

    Bs4

--- Aula185.py
+++ Aula185.py
@@ -7,13 +7,18 @@
 # - Doc: https://www.crummy.com/software/BeautifulSoup/bs4/doc.ptbr/
 # + Instalação
 # - pip install requests types-requests bs4
+
+
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://codepen.io/luizomf/full/KKNwMeG'
+url = 'http://127.0.0.1:5500/projeto/index.html'
 response = requests.get(url)
 raw_html = response.text
 parsed_html = BeautifulSoup(raw_html, 'html.parser')
+
+# no parsed_html pode se adicionar no final o comand from_encoding='utf-8'
+# caso esteja com problemas de caracteres.
 
 # print(parsed_html.title.text)
 
@@ -21,3 +26,7 @@ top_jobs_heading = parsed_html.select_one('#intro > div > div > article > h2')
 
 if top_jobs_heading is not None:
     article = top_jobs_heading.parent
+
+    if article is not None:
+        for p in article.select('p'):
+            print(p.text)
